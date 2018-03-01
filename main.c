2156b6d35f3ab208027f474b11b1e61eb525b2fc
Sergio Prado <sergio.prado@e-labworks.com>
2018-03-01 11:45:14 -0300

    adding sum function
    Signed-off-by: Sergio Prado <sergio.prado@e-labworks.com>

--- main.c
+++ main.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 
+int sum(int a, int b)
+{
+	return a + b;
+}
+
 int main(int argc, const char *argv[])
 {
 	printf("Hello Fantastic World!\n");
+	printf("Sum is %d\n", sum(10, 20));
 	return 0;
 }
